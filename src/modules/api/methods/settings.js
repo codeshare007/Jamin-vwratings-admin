f212571d0801c63ff96ecb461fdd294e8f6e0763
robertj1029 <robertj9216@gmail.com>
2021-12-17 07:12:59 +0400

    Add update

--- src/modules/api/methods/settings.js
+++ src/modules/api/methods/settings.js
@@ -7,5 +7,12 @@ export default axios => ({
   },
   update(id, payload) {
     return axios.put(`settings/${id}`, payload)
+  },
+
+  getAnnouncement() {
+    return axios.get('settings/announcement')
+  },
+  setAnnouncement(payload) {
+    return axios.post('settings/announcement', {announcement_html: payload})
   }
 });
--- src/views/settings/Settings.vue
+++ src/views/settings/Settings.vue
@@ -6,19 +6,30 @@
         <b-card header="Settings">
           <b-table :items="settings" :fields="settingFields">
             <template #cell(index)="row">
-              {{ row.index + 1}}
+              {{ row.index + 1 }}
             </template>
             <template #cell(actions)="row">
               <b-button variant="primary" size="sm" @click="settingEdit(row.item.id)">
-                <b-icon-pencil />
+                <b-icon-pencil/>
               </b-button>
             </template>
           </b-table>
         </b-card>
+
+        <b-card header="Announcement" class="mt-4">
+          <b-form>
+            <b-form-group>
+              <b-form-textarea rows="5" v-model="announcement_html"/>
+            </b-form-group>
+            <div class="d-flex justify-content-end mt-3">
+              <b-button variant="success" @click="setAnnouncement">Save</b-button>
+            </div>
+          </b-form>
+        </b-card>
       </b-col>
       <b-col cols="6">
         <b-card header="Ads Campaigns">
-          <b-table :items="campaigns" :fields="campaignFields" />
+          <b-table :items="campaigns" :fields="campaignFields"/>
         </b-card>
       </b-col>
     </b-row>
@@ -30,6 +41,7 @@ import {mapActions} from "vuex";
 export default {
   data() {
     return {
+      announcement_html: null,
       settings: [],
       campaigns: [],
       settingFields: [
@@ -52,10 +64,10 @@ export default {
   mounted() {
     this.fetchSettings();
     this.fetchAdsCampaigns();
+    this.getAnnouncement();
   },
 
   methods: {
-
     ...mapActions({
       editSetting: 'dialogs/setting/edit'
     }),
@@ -69,6 +81,18 @@ export default {
     fetchAdsCampaigns() {
       this.$api.adsCampaigns.fetch().then(response => {
         this.campaigns = response.data.data
+      })
+    },
+
+    setAnnouncement() {
+      this.$api.settings.setAnnouncement(this.announcement_html).then(() => {
+        this.getAnnouncement()
+      })
+    },
+
+    getAnnouncement() {
+      this.$api.settings.getAnnouncement().then(response => {
+        this.announcement_html = response.data;
       })
     },
 
