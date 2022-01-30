5c3e017c01b5f1b7f49be606c3c239b6a2ebb4e1
robertj1029 <robertj9216@gmail.com>
2022-01-30 05:06:11 -0500

    make dashboard cards link to admin pages

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,58 +2,78 @@
   <div class="adminDashboard">
     <b-row v-if="!loading">
       <b-col>
+	<a href='users'>
         <b-card class="mt-2" header="Members">
           <h1>{{ information.users }}</h1>
         </b-card>
+	</a>
       </b-col>
       <b-col>
+	<a href='parties'>	  
         <b-card class="mt-2" header="Parties">
           <h1>{{ information.parties }}</h1>
         </b-card>
+	</a>		
       </b-col>
       <b-col>
+	<a href='avis'>	  
         <b-card class="mt-2" header="Players">
           <h1>{{ information.avis }}</h1>
         </b-card>
+	</a>		
       </b-col>
       <b-col>
+	<a href='messages'>	  
         <b-card class="mt-2" header="Messages">
           <h1>{{ information.messages }}</h1>
         </b-card>
+	</a>		
       </b-col>
     </b-row>
     <b-row v-if="!loading">
       <b-col>
+	<a href='parties-claims'>	  
         <b-card class="mt-2" header="Parties Claimed">
           <h1>{{ information.pclaims }}</h1>
         </b-card>
+	</a>		
       </b-col>
       <b-col>
+	<a href='parties-comments'>	  
         <b-card class="mt-2" header="Parties Comments">
           <h1>{{ information.pcomments }}</h1>
         </b-card>
+	</a>		
       </b-col>
       <b-col>
+	<a href='parties-ratings'>	  
         <b-card class="mt-2" header="Parties Ratings">
           <h1>{{ information.pratings }}</h1>
         </b-card>
+	</a>		
       </b-col>
     </b-row>
     <b-row v-if="!loading">
       <b-col>
+	<a href='avis-claims'>	  
         <b-card class="mt-2" header="Players Claimed">
           <h1>{{ information.claims }}</h1>
         </b-card>
+	</a>		
       </b-col>
       <b-col>
+	<a href='avis-comments'>	  
         <b-card class="mt-2" header="Players Comments">
           <h1>{{ information.comments }}</h1>
         </b-card>
+	</a>		
       </b-col>
       <b-col>
+	<a href='avis-ratings'>	  
         <b-card class="mt-2" header="Players Ratings">
           <h1>{{ information.ratings }}</h1>
         </b-card>
+	</a>		
       </b-col>
     </b-row>
   </div>
